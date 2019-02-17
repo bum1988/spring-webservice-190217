eb14a5c76d40011d8d0d262f38844887addc3e88
bum1988 <bum1988@naver.com>
2019-02-17 13:57:21 +0900

    first commit

---
+++ src/main/java/com/yb/webservice/web/WebRestController.java
@@ -0,0 +1,18 @@
+package com.yb.webservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "HelloWorld";
+	}
+
+	@GetMapping("/health")
+	public String health() {
+		return "Service works well";
+	}
+}
