827e224a72a2eb6a420e95f0210fefab2cddbb0c
bum1988 <bum1988@naver.com>
2019-03-11 20:51:09 +0900

    Update WebRestController.java

--- src/main/java/com/yb/webservice/web/WebRestController.java
+++ src/main/java/com/yb/webservice/web/WebRestController.java
@@ -9,7 +9,7 @@ public class WebRestController {
 
 	@RequestMapping("/")
 	public String hello() {
-		return "Hello KaKao Team!!";
+		return "Hello KaKaoPay Team!!";
 	}
 
 	@RequestMapping("/health")
